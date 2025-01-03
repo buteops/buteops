651170ca4927bffc10ce0278571db84030c12f87
sapategu <adalgisoabercio@gmail.com>
2025-01-03 17:05:30 +0700

    feat: Initial buteops commit

---
+++ ingestion/__init__.py
@@ -0,0 +1,4 @@
+from mlopsency.core import Core       #noqa: F401
+from mlopsency.data import Data       #noqa: F401
+from mlopsency.design import Design   #noqa: F401
+from mlopsency.ops import Ops         #noqa: F401
---
+++ ingestion/ops.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+# from mlopsency import Core
+
+
+class Ops:
+  pass
+
+class Inference:
+  pass
+
+class Docker:
+  pass
+
+class Storage:
+  pass
+
+class Target:
+  pass
+
+class API:
+  pass
+
+class Tiny:
+  pass
